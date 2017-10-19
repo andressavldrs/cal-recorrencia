0ee7d97e2005ad9d45da27cfaec7a44a4ff67490
andressavldrs <andressarodrial@gmail.com>
2017-10-19 11:26:53 -0200

    Calculo do tempo medio de execucao

--- run.sh
+++ run.sh
@@ -7,15 +7,25 @@ out=$1
 function exec {
 for j in ${n[@]}; 
 	do 
+		duration=0
 		for x in {1..10}
 		do
-			./$out $1 $2 $3 $4 $5 $6 $7 $j
+			#Calcula o tempo de execucao e nao permite que ultrapasse
+			#6 minutos
+			start=`date +%s%3N`
+			timeout 6m ./$out $1 $2 $3 $4 $5 $6 $7 $j
+			end=`date +%s%3N`
+			((duration+=end-start))
+			#echo $((runtime/1000000))
 		done
+		# Tempo medio de execucao
+		echo "TME: ((duration/10)) ms"
+
 	done
 }
 
 
-for g in {1..3};
+for g in {0..3};
 	do 
 		exec 1  2 0 0 1 0 $g
 		exec 2  2 0 0 1 0 $g
